5f69c0e3cc48fd0b92ad709df8bb398b2ad84663
Mateusz Wenus <mateusz.wenus@gmail.com>
2021-11-18 22:50:16 +0100

    added timeout config for calls to Github API

--- src/main/java/com/github/mateuszwenus/github_repo_info_webflux/Application.java
+++ src/main/java/com/github/mateuszwenus/github_repo_info_webflux/Application.java
@@ -1,17 +1,26 @@
 package com.github.mateuszwenus.github_repo_info_webflux;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 public class Application {
 
 	@Value("${github.api.url}")
 	private String githubApiUrl;
+	@Value("${github.api.timeoutMillis}")
+	private int timeoutMillis;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -19,8 +28,13 @@ public class Application {
 
 	@Bean
 	public WebClient webClient() {
+		HttpClient httpClient = HttpClient.create()
+			.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeoutMillis)
+			.doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(timeoutMillis, TimeUnit.MILLISECONDS)));
+
 		return WebClient.builder()
 				.baseUrl(githubApiUrl)
+				.clientConnector(new ReactorClientHttpConnector(httpClient))
 				.defaultHeader(HttpHeaders.ACCEPT, "application/vnd.github.v3+json")
 				.build();
 	}
