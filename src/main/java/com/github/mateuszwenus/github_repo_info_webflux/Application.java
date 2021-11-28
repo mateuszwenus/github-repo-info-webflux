b9f016b0a28fb05f34e11b8c67219bede7ea6288
Mateusz Wenus <mateusz.wenus@gmail.com>
2021-11-28 10:11:06 +0100

    added logging of HTTP calls and Mono statuses

--- src/main/java/com/github/mateuszwenus/github_repo_info_webflux/Application.java
+++ src/main/java/com/github/mateuszwenus/github_repo_info_webflux/Application.java
@@ -1,21 +1,30 @@
 package com.github.mateuszwenus.github_repo_info_webflux;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+import static net.logstash.logback.argument.StructuredArguments.v;
+
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 public class Application {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Value("${github.api.url}")
 	private String githubApiUrl;
@@ -36,6 +45,24 @@ public class Application {
 				.baseUrl(githubApiUrl)
 				.clientConnector(new ReactorClientHttpConnector(httpClient))
 				.defaultHeader(HttpHeaders.ACCEPT, "application/vnd.github.v3+json")
+				.filters(exchangeFilterFunctions -> {
+					exchangeFilterFunctions.add(logRequest());
+					exchangeFilterFunctions.add(logResponse());
+				})
 				.build();
+	}
+
+	private ExchangeFilterFunction logRequest() {
+		return ExchangeFilterFunction.ofRequestProcessor(req -> {
+			logger.info("request {} {} {}", kv("logPrefix", req.logPrefix()), v("method", req.method()), v("url", req.url()));
+			return Mono.just(req);
+		});
+	}
+
+	private ExchangeFilterFunction logResponse() {
+		return ExchangeFilterFunction.ofResponseProcessor(resp -> {
+			logger.info("response {} {}", kv("logPrefix", resp.logPrefix()), kv("status", resp.statusCode()));
+			return Mono.just(resp);
+		});
 	}
 }
--- src/main/java/com/github/mateuszwenus/github_repo_info_webflux/GithubRepositoryInfoService.java
+++ src/main/java/com/github/mateuszwenus/github_repo_info_webflux/GithubRepositoryInfoService.java
@@ -1,5 +1,12 @@
 package com.github.mateuszwenus.github_repo_info_webflux;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+import static net.logstash.logback.argument.StructuredArguments.v;
+
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,6 +16,7 @@ import reactor.core.publisher.Mono;
 @Service
 public class GithubRepositoryInfoService implements RepositoryInfoService {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private final WebClient webClient;
 
 	public GithubRepositoryInfoService(WebClient webClient) {
@@ -23,6 +31,22 @@ public class GithubRepositoryInfoService implements RepositoryInfoService {
 				.onStatus(HttpStatus.NOT_FOUND::equals, clientResponse -> Mono.error(new RepositoryNotFoundException()))
 				.onStatus(HttpStatus::isError, clientResponse -> Mono.error(new RepositoryInfoServiceException()))
 				.bodyToMono(GithubResponse.class)
-				.map(RepositoryInfo::new);
+				.map(RepositoryInfo::new)
+				.doOnSuccess(logSuccess("getRepositoryInfo"))
+				.doOnError(logError("getRepositoryInfo"))
+				.doOnCancel(logCancel("getRepositoryInfo"));
+	}
+
+	private <T> Consumer<T> logSuccess(String name) {
+		return it -> logger.info("{} ended with {}", kv("operation", name), kv("status", "success"));
+	}
+
+	private Consumer<Throwable> logError(String name) {
+		return exc -> logger.info("{} ended with {}: ({}: {})", kv("operation", name), kv("status", "error"),
+				v("excClass", exc.getClass().getCanonicalName()), v("excMessage", exc.getMessage()));
+	}
+
+	private Runnable logCancel(String name) {
+		return () -> logger.info("{} ended with {}", kv("operation", name), kv("status", "cancel"));
 	}
 }
